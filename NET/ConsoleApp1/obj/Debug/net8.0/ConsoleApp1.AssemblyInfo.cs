1fe5e38bcf5906024e1cc0530d360d95ee71945c
josmartinezbe <sebastianmartinez66@gmail.com>
2025-10-03 17:36:40 -0500

    hagoo nuevas rutas y verifico que funcione el sript

--- NET/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ NET/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79056e750af40b5b59d1d880f7ace835aef17248")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91a34cd44b58af5905cc52caa2bc63d983a6bd4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
