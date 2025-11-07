db02d5cdab092c8314d9990f20e581c521d296b4
josmartinezbe <sebastianmartinez66@gmail.com>
2025-11-07 11:45:10 -0500

    y para archivos .xlsx que mantiene los dominos

--- NET/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ NET/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62f42d0fb18527d45e28529829621ed7b2e0d93d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+476079624df35fd8a95e4dbc71f455bc47601925")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
