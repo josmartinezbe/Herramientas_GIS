c0b5c26cf3085d2d6b6055b481fe3f216f25e5e9
josmartinezbe <sebastianmartinez66@gmail.com>
2025-11-07 11:44:48 -0500

    nueva versión del script para arcmap y arcgis pro

--- NET/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ NET/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15d3c8f454ad4279accf7a099c36e3d34938f626")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62f42d0fb18527d45e28529829621ed7b2e0d93d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
