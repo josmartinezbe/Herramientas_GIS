d98f51f1772c7caf954b0010c14010ff92642b64
josmartinezbe <sebastianmartinez66@gmail.com>
2025-11-08 19:05:43 -0500

    finales

--- NET/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ NET/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+476079624df35fd8a95e4dbc71f455bc47601925")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f16916cdd3dbc513740c26547ca01a30019de506")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
